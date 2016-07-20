af5165758e721f9fc42f8ec6eedadb8e4d9c4522
hagai-lvi <hagai.lvi@gmail.com>
2016-07-20 21:35:53 +0300

    Fix main class

---
+++ dsp-final-project-step-1/src/main/java/dsp/step1/Step1Mapper.java
@@ -0,0 +1,10 @@
+package dsp.step1;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class Step1Mapper extends Mapper<Object, Text, Text, LongWritable> {
+
+}
